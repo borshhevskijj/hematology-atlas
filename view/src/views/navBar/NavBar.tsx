866cc07e3c5ff88493e8c3db7af1685a463ab6bf
lemonsqueezy <borshhevskijj98@mail.ru>
2023-07-03 21:16:41 +0300

    bug with background position are fixed

--- view/src/views/navBar/NavBar.tsx
+++ view/src/views/navBar/NavBar.tsx
@@ -70,6 +70,7 @@ const NavBar: React.FC<Props> = ({ isOpen, SubmenuRef }) => {
         <span />
       </div>
       <nav className={`${isBrgMenuOpen ? `${cl.menuActive} ${cl.menu}` : cl.menu}`}>
+        {isBrgMenuOpen && <BackgroundLines />}
         <ul>
           {menuLinks.mainMenu.map(({ path, name }) => {
             return (
